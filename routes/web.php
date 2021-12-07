258b29ca678343f0ec89879e926b45cffe3f2b8c
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-12-07 19:51:17 +0100

    test

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Inertia\Inertia;
 app()->setLocale(request()->subdomain());
 
 Route::get('/', function () {
-    return config('localized-routes.supported-locales.it');
+    return config('localized-routes.supported-locales.it') . 'test';
 })->name('home');
 
 Route::get('/locale', function () {
