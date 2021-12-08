9eff7a7e8ea2d7d4edccc6a2b388bebf9aba7e87
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-12-08 18:21:47 +0100

    fix config

--- config/localized-routes.php
+++ config/localized-routes.php
@@ -6,8 +6,8 @@ return [
      * The locales you wish to support.
      */
     'supported-locales' => [
-        'it' => env('domain'),
-        'en' => 'en.' . env('domain'),
+        'it' => env('DOMAIN'),
+        'en' => 'en.' . env('DOMAIN'),
     ],
 
     /**
